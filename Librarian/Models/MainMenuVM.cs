aa6cda5dcf1c14140a010f9ce89e593db4b549e4
Golda Fast <golda.fast@hpe.com>
2015-12-08 14:42:55 +0200

    EDMX for DB, among lots of initial stuff

---
+++ Librarian/Models/MainMenuVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Librarian.Models
+{
+    public class MainMenuVM
+    {
+        public IEnumerable<MenuItem> MenuItems { get; set; }
+
+    }
+}
---
+++ Librarian/Models/MenuItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Librarian.Models
+{
+    public class MenuItem
+    {
+        public string IconClass { get; set; }
+        public string Text { get; set; }
+        public string Controller { get; set; }
+        public string Action { get; set; }
+        public Dictionary<string,string> Args { get; set; }
+
+    }
+}
